57ce98d38e8e00933cd6a18ae2470bbf68694647
Trinhdvt <trinhvideo123@gmail.com>
2020-06-29 10:34:01 +0700

    Modified db path

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -52,7 +52,7 @@ public class Database {
     public void connectDB() throws ClassNotFoundException, SQLException {
         if (cnn == null) {
             Class.forName("org.sqlite.JDBC");
-            cnn = DriverManager.getConnection("jdbc:sqlite:DB/" + DB_NAME + ".db");
+            cnn = DriverManager.getConnection("jdbc:sqlite:db/" + DB_NAME + ".db");
             createDB();
         }
     }
